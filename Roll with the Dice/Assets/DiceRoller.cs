20b9d5a152d33e7e4c05d90388aa6287cd583acd
Xargen-Github <vanherbergenx@gmail.com>
2022-07-15 23:34:23 +0200

    Added a dice rolling scene

---
+++ Roll with the Dice/Assets/DiceRoller.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceRoller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(RollDice());
+    }
+
+    public int RollDice(int faces = 6) {
+        return Random.Range(1, faces + 1);
+    }
+}
