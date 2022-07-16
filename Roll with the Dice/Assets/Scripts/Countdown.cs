e4480f0e3359377d5d27278773233d753b365861
SimonWilmotsPXL <simon.wilmots1@gmail.com>
2022-07-16 21:51:25 +0200

    Added a countdown + time controller

---
+++ Roll with the Dice/Assets/Scripts/Countdown.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour {
+    public float countTime = 60;
+    public Image timerBar;
+
+    float elapsedTime = 0f;
+    float lerpSpeed;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        elapsedTime += Time.deltaTime;
+
+        lerpSpeed = Time.deltaTime;
+        timerBar.fillAmount = 1 - (elapsedTime / countTime);
+	}
+}
