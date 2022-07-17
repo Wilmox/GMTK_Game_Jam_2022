47d3a615e080864188ca50a4d728f0908806da41
SimonWilmotsPXL <simon.wilmots1@gmail.com>
2022-07-17 17:12:32 +0200

    Endscreen added

--- Roll with the Dice/Assets/Scripts/MainMenu.cs
+++ Roll with the Dice/Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         //SceneManager.LoadScene("Name");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("World Building");
     }    
 
     public void QuitGame()
